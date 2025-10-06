96d4bb02e0bfafd067f86b48cba89eaf0847070c
fjt-dev <157100279+fjt-dev@users.noreply.github.com>
2025-10-06 10:16:49 +0900

    React interactivity: filtering, conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
+  const [filter, setFilter] = useState("All");
+
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false};
     setTasks([...tasks, newTask]);
@@ -32,7 +42,9 @@ function App(props) {
     setTasks(editedTaskList);
   }
   const[tasks, setTasks] = useState(props.tasks);
-  const taskList = tasks?.map((task) => (
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map((task) => (
     <Todo
       id={task.id}
       name={task.name}
@@ -43,6 +55,14 @@ function App(props) {
       editTask={editTask}
     />
   ));
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
@@ -50,9 +70,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
